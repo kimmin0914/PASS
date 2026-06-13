3a68948f6b25026e8d4bcb2273d1aa89c29309fe
kimmin0914 <zktpxldk@gmail.com>
2026-06-13 17:39:47 +0900

    패스 활동

---
+++ Star/FileName.c
@@ -0,0 +1,164 @@
+﻿/*#include <stdio.h>
+int main(void)
+{
+	for (int i = 0; i < 5; i++) // 삼각형 높이 5칸
+	{
+			for (int j = 0; j <= i; j++) // 별 갯수 아래로 갈수록 한 개씩 증가
+			{
+				printf("*");
+			}
+			printf("\n"); // 출력 후 줄 바꿈
+	}
+
+	return 0;
+}*/
+
+/*#include <stdio.h>
+int main(void)
+{
+	for (int i = 5; i > 0; i--) // 역삼각형 높이 5칸
+	{
+		for (int j = 1; j <= i; j++) // 별 갯수 아래로 갈수록 한 개씩 감소
+		{
+			printf("*");
+		}
+		printf("\n"); // 별이 출력되고 줄 바꿈
+	}
+
+	return 0;
+}*/
+
+/*#include <stdio.h>
+int main(void)
+{
+	for (int i = 0; i < 5; i++) // 높이 5칸
+	{
+		for (int j = 4; j > i; j--) // 첫 시작은 4칸 띄우기 아래로 갈수록 한 칸씩 감소
+		{
+			printf(" ");
+		}
+		for (int k = 0; k <= i; k++) // 별 갯수 아래로 갈수록 한 개씩 증가
+		{
+			printf("*");
+		}
+		printf("\n"); // 별이 출력되고 줄 바꿈
+	}
+	return 0;
+}*/
+
+/*#include <stdio.h>
+int main(void)
+{
+	for (int i = 0; i < 6; i++)
+	{
+		for (int j = 4; j >= i; j--)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k <= 2 * i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}*/
+
+/*#include <stdio.h>
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = - 1; j >= - i; j--)
+		{
+			printf(" ");
+		}
+		for (int k = 10; k >= 2 * i; k--)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}*/
+
+/*#include <stdio.h>
+int main(void)
+{
+	for (int i = 0; i < 5; i++) // 삼각형 높이 6칸
+	{
+		for (int j = 4; j >= i; j--) // 첫 시작은 5칸 띄우기 아래로 갈수록 감소
+		{
+			printf(" ");
+		}
+		for (int k = 0; k <= 2 * i; k++) // 별 갯수 아래로 갈수록 두 개씩 증가
+		{
+			printf("*");
+		}
+		printf("\n"); // 별이 출력되고 줄 바꿈
+	}
+	for (int i = 0; i < 6; i++) // 역삼각형 높이 6칸
+	{
+		for (int j = -1; j >= -i; j--) // 첫 시작은 0칸 띄우기 아래로 갈수록 증가
+		{
+			printf(" ");
+		}
+		for (int k = 10; k >= 2 * i; k--) // 별 갯수 아래로 갈수록 두 개씩 감소
+		{
+			printf("*");
+		}
+		printf("\n"); // 별이 출력되고 줄 바꿈
+	}
+	return 0;
+}*/
+
+/*#include <stdio.h>
+int main(void)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d\n", i);
+		for (int j = 0; j <= 5; j++)
+		{
+			printf("%d", j);
+		};
+	}
+
+	return 0;
+}*/
+
+/*#include <stdio.h>
+int main(void)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d", i);
+	}
+
+	return 0;
+}*/
+
+#include <stdio.h> 
+int main(void)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 4; j > i; j--)
+			printf(" ");
+
+		for (int k = 1; k <= 2 * i + 1; k++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	for (int i = 3; i >= 0; i--)
+	{
+		for (int j = 4; j > i; j--)
+			printf(" ");
+		for (int k = 1; k <= 2 * i + 1; k++)
+			printf("*");
+		printf("\n");
+	}
+	return 0;
+}
---
+++ Up&Down-Game/game.c
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h> // 랜덤한 숫자를 만드는 도구함
+#include <time.h> // 현재 시간을 알아내는 도구함 랜덤 쓰려면 필수
+
+int main()
+{
+	srand(time(NULL)); // 랜덤으로 숫자를 설정하게 함
+
+	int num1 = rand() % 100 + 1; // 1~100 까지 범위 설정
+	int num2;
+	int chances = 5; // 횟수 제한 (예: 5번)
+
+	printf("1~100 사이의 랜덤한 숫자를 정했습니다.\n");
+	printf("총 %d번의 기회가 주어집니다!\n\n", chances);
+
+	// 기회가 0보다 클 때 반복
+	while (chances > 0)
+	{
+		printf("남은 기회: %d번\n", chances);
+		printf("숫자를 맞춰보세요: ");
+		scanf("%d", &num2);
+
+		if (num1 == num2) // 정한 숫자랑 입력한 숫자가 같으면 멈춤
+		{
+			printf("정답!\n");
+			break;
+		}
+		else if (num1 > num2) // 정한 숫자가 크면 Up 출력
+		{
+			printf("Up!\n\n");
+		}
+		else // 정한 숫자가 작으면 Down 출력
+		{
+			printf("Down!\n\n");
+		}
+
+		chances--; // 틀리면 1회 감소
+	}
+
+	// 횟수를 다 차감하면 출력
+	if (chances == 0)
+	{
+		printf("기회를 모두 소진했습니다. 정답은 %d였습니다!\n", num1);
+	}
+
+	return 0;
+}
